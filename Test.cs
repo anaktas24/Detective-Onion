7e6e8ae114c3ac0807a46df465d59270959b7a30
Ioannis Antoniou <johnantony24@hotmail.com>
2023-02-12 20:57:53 +0100

    Preferences

---
+++ Test.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Training : MonoBehaviour
+{
+    private void private void Start() {
+        Console.write("few")
+    }
+}
