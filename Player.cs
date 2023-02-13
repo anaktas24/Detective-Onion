2e3e7bc20a4c66b119f48698f2a79f2ae6abfbfd
Ioannis Antoniou <johnantony24@hotmail.com>
2023-02-13 16:54:02 +0100

    Player Controller

---
+++ Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float moveSpeed = 7;
+    // Update is called once per frame
+    private void Update(){
+        Vector2 inputVector = new Vector2(0,0);
+
+        if (Input.GetKey(KeyCode.W)){
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S)){
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A)){
+            inputVector.x = +1;
+        }
+        if (Input.GetKey(KeyCode.D)){
+            inputVector.x = -1;
+        }
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVectorx, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+}
