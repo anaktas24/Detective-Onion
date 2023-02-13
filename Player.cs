584edd05db00ecbac64fce3e85f47b4a2d99573b
Ioannis Antoniou <johnantony24@hotmail.com>
2023-02-13 19:17:25 +0100

    Player Controller Rotatiom

--- Player.cs
+++ Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 7;
+    [SerializeField] private float moveSpeed = 7;
     // Update is called once per frame
     private void Update(){
         Vector2 inputVector = new Vector2(0,0);
@@ -15,15 +15,17 @@ public class NewBehaviourScript : MonoBehaviour
         if (Input.GetKey(KeyCode.S)){
             inputVector.y = -1;
         }
-        if (Input.GetKey(KeyCode.A)){
+        if (Input.GetKey(KeyCode.D)){
             inputVector.x = +1;
         }
-        if (Input.GetKey(KeyCode.D)){
+        if (Input.GetKey(KeyCode.A)){
             inputVector.x = -1;
         }
         inputVector = inputVector.normalized;
 
-        Vector3 moveDir = new Vector3(inputVectorx, 0f, inputVector.y);
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
     }
 }
